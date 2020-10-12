733e76af19829d3ce3aea284ab563ba06c4175f9
Robert Laverty <roblav96@gmail.com>
2020-10-12 05:40:12 -0400

    Update Generator.java

--- dts-generator/src/main/java/com/telerik/dts/Generator.java
+++ dts-generator/src/main/java/com/telerik/dts/Generator.java
@@ -95,6 +95,7 @@ public class Generator {
         for (File file : jars) {
             if (file.exists()) {
                 if (file.isFile() && file.getName().endsWith(".jar")) {
+                    System.out.println("▶ -input -> " + file);
                     JarFile jar = JarFile.readJar(file.getAbsolutePath());
                     ClassRepo.cacheJarFile(jar);
                 } else if (file.isDirectory()) {
@@ -115,6 +116,7 @@ public class Generator {
         for (File file : jars) {
             if (file.exists()) {
                 if (file.isFile() && file.getName().endsWith(".jar")) {
+                    System.out.println("▶ -super -> " + file);
                     JarFile jar = JarFile.readJar(file.getAbsolutePath());
                     ClassRepo.cacheSuperJarFile(jar);
                 } else if (file.isDirectory()) {
